9b9990b86c426c778d59d6f6c9ce5c1123ef41be
Ben <benjamin.a.spencer@gmail.com>
2015-10-20 09:27:35 -0400

    Response to Assessment

--- challenge.rb
+++ challenge.rb
@@ -4,9 +4,9 @@
 # Question 1: Ruby objects and classes.
 # How would one find the class of the below objects.
 
-"Hello World"
-33
-65.89
+"Hello World".class
+33.class
+65.89.class
 
 # Question 2: Booleans
 # What would be the result of executing the below statements
@@ -17,18 +17,31 @@ else
   puts "it's false"
 end
 
+The result would be "it's true"
+
 # 2.1 Substitue "hello" && 4.5 for 33 && 55 above. What's the output?
+  "it's true"
+
 # 2.2 Substitue nil && 88 for 33 && 55 above. What's the output?
+  "it's false"
+
 # 2.4 Substitue 63.4 < 64. What's the output?
+  "it's true"
+
 
 # Question 3 :Strings
 
 # How would you (Yes, you can look up the String methods!):
 
-# 3.1. determine if a string is included another another string?
+# 3.1. determine if a string is includes another string?
+  .include? "substring"
+
 # 3.2. determine if the length of a string?
+  "string".length OR "string".size
+
 # 3.3. Go from the string "eels in my hovercraft" to "fish in my sink" but don't
 # change the string "eels in my hovercraft"
+"eels in my hovercraft".gsub!(/eels in my hovercraft/, "fish in my sink")
 
 
 # Question 4 : Variables
@@ -38,12 +51,21 @@ end
 vehicle = "4 wheel Bus"
 
 # 4.1 to "6 wheel Bus"
+vehicle.gsub!(/4/, "6")
+
+
 # 4.2 to "6 wheel tank"
+vehicle.gsub!(/Bus/, "tank")
+
+
 # 4.3 Create a NEW string from the string in 4.2 that will be
 # "6 wheel tank that shoots rainbow bunnnies"
 
-# Question 5 : Loop
-# - write a loop that guesses your age.
+new_vehicle = vehicle << " that shoots rainbow bunnies"
+
+
+Question 5 : Loop
+- write a loop that guesses your age.
 # - Only allow 4 guesses
 # - Notify the user if they guess correctly OR if they run out of turns to guess
 # - Get mad and curse the fingers that thought you where that old,
@@ -51,7 +73,29 @@ vehicle = "4 wheel Bus"
 # - I'm not kidding, say unkind works to a user that thinks your 10 years older
 #   than you are.
 
-# Bonus Question: Who said this?
+number_of_guesses = 0
+my_age = 26
+
+while number_of_guesses < 4
+  print "Can you guess my age?"
+  user_input = gets.chomp!.to_i
+  number_of_guesses += 1
+    if user_input == my_age
+    print "You got it right!\n"
+    elsif (user_input > my_age +10) && number_of_guesses <= 3
+    print "\nYou should be bad about yourself. Guess again.\n"
+    else
+    print "You ran out of guesses!\n"
+    end
+  break if user_input == my_age || (number_of_guesses >= 4)
+end
+
+
+
+
+
+
+# Bonus Question: Who said this? J.F.K. (ah, the wonders of Google)
 
 # The great enemy of the truth is very often not the lie -- deliberate, contrived
 # and dishonest, but the myth, persistent, persuasive, and unrealistic.
